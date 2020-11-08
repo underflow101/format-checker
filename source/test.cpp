0a1b19a1f77fed085d2d2eca631f230621b6c63e
underflow101 <ikarus125@gmail.com>
2020-11-08 21:47:36 +0900

    feat: Add v0.1
    - Finally, v0.1 for format-checker in clang-format 3.9
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- source/test.cpp
+++ source/test.cpp
@@ -1,20 +1,11 @@
-TEST(ArgMaxTest, UnsupportedType_NEG) {
-  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>({1, 1, 2, 4}, {
-                                                                             1,
-                                                                             2,
-                                                                             7,
-                                                                             8,
-                                                                             1,
-                                                                             9,
-                                                                             7,
-                                                                             3,
-                                                                         });
-  Tensor dimension_tensor = makeInputTensor<DataType::S32>({}, {3});
-  Tensor output_tensor = makeOutputTensor(DataType::U8);
+#include <iostream>
 
-  ArgMaxParams params{};
-  params.output_type = DataType::U8;
-  ArgMax kernel(&input_tensor, &dimension_tensor, &output_tensor, params);
-  kernel.configure();
-  EXPECT_ANY_THROW(kernel.execute());
+void main() {
+  int i = 0;
+  cout << "i: " << i
+       << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          "aaaaaaaaaaaaaaaaaaaAAAAAA"
+       << endl;
+
+  return;
 }
