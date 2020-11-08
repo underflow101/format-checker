1f98151d552eb81fee66ad8e7caeba29cc29e523
underflow101 <ikarus125@gmail.com>
2020-11-08 21:26:26 +0900

    feat: Initial commit
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ source/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void main()                                                                                           {
+            int i = 0; 
+    cout << "i: "             <<               i                  <<                        endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    return;
+}
