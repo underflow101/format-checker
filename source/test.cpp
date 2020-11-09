4481d436e94965bc517131abce4d45daeed8a240
underflow101 <ikarus125@gmail.com>
2020-11-09 23:50:48 +0900

    fix: Fix for more variant
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- source/test.cpp
+++ source/test.cpp
@@ -4,8 +4,7 @@ void main() {
   int i = 0;
   cout << "i: " << i
        << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-          "aaaaaaaaaaaaaaaaaaaAAAAAA"
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
        << endl;
-
   return;
 }
